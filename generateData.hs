e25722ae204f8a5d6adf62efe3586b5a601b48be
Matthew Filmer <mattfilmer@gmail.com>
2014-02-13 16:04:26 -0500

    fixed problem with bandEdges, it works now

--- generateData.hs
+++ generateData.hs
@@ -1,30 +1,33 @@
 import Newton
 import KP
 
-equ = charEqu 5e-10 1e-10 (evToJ 1.4)
+equ = charEqu 5e-10 1e-10 1.4
 
+-- main = mapM_ putStrLn [show x ++ ", " ++ show (equ x) | x <- [0.01, 0.02 .. 50]]
 main = mapM_ putStrLn [show x ++ ", " ++ show (equ x) | x <- [0.01, 0.02 .. 50]]
+
+bandEdges = [getEdge equ (x + 0.000001) | x <- 0:bandEdges]
 
 -- getEdge takes a function and a starting point
 -- and returns the next band edge
 getEdge :: (Double -> Double) -> Double -> Double
 getEdge func start = edge
   where
-    edge = solve func (approxNextEdge func start)
+    edge = solve (\x -> equ x - (signum (func x))) (approxNextEdge func start)
 
 approxNextEdge :: (Double -> Double) -> Double -> Double
 approxNextEdge func start =
   fst $ head $ dropWhile (\(_, x) -> signum (x - edgeSign) == firstSign) funcVals
     where
       edgeSign
-        | startSlope == 1 && startVal > 0 = 1
-        | startSlope == 1 && startVal < 0 = (-1)
-        | startSlope == -1 && startVal < 0 = (-1)
-        | startSlope == -1 && startVal > 0 = 1
+        | startSlope == 1 && startVal >= -1 = 1
+        | startSlope == 1 && startVal < -1 = (-1)
+        | startSlope == -1 && startVal <= 1 = (-1)
+        | startSlope == -1 && startVal > 1 = 1
           where
             startVal = func start
-            startSlope = signum $ slope func start
+            startSlope = round $ signum $ slope func start
       firstSign = signum $ snd (head funcVals) - edgeSign
       funcVals = zip xs (map func xs)
         where
-          xs = [start, start + 0.0001 ..]
+          xs = [start, start + 0.001 ..]
