33934f0fc3ba0e261304cf5324058e71dc8a7d67
Matthew Filmer <mattfilmer@gmail.com>
2014-02-14 01:06:19 -0500

    Having some trouble generating all the 1st gap

--- generateData.hs
+++ generateData.hs
@@ -1,23 +1,35 @@
 import Newton
 import KP
 
-equ = charEqu 5e-10 1e-10 1.4
 
--- main = mapM_ putStrLn [show x ++ ", " ++ show (equ x) | x <- [0.01, 0.02 .. 500]]
-main = mapM_ putStrLn [show x ++ ", " ++ show (bandGaps !! x) ++ ", " ++ show (allowedBands !! x)| x <- [0, 1 .. 20]]
+-- First band gap energy for a variety of values of a
+-- aVals = logspace 5e-10 1e-8 11
+aVals = linspace 1e-10 1e-9 10
+aEqus = map (\a x -> charEqu a 1e-10 1.4 x) aVals
+getFirstGap equ = diffs (tail edges) !! 0
+  where
+    edges = [getEdge equ (x + 0.000001) | x <- 0:edges]
+firstGaps = map getFirstGap aEqus
+-- main = mapM_ putStrLn [show val ++ ", " ++ show equ | (val,equ) <- zip aVals firstGaps]
 
-bandEdges = [getEdge equ (x + 0.000001) | x <- 0:bandEdges]
-
-diffs (a:b:xs) = (b - a):(diffs xs)
+-- Band energies for the nth band
+simpleEqu = charEqu 1e-10 1e-10 1.4
+bandEdges = [getEdge simpleEqu (x + 0.000001) | x <- 0:bandEdges]
 bandGaps = diffs (tail bandEdges)
 allowedBands = diffs bandEdges
+main = mapM_ putStrLn [show x ++ ", " ++ show (simpleEqu x) | x <- [0.01, 0.02 .. 500]]
+-- main = mapM_ putStrLn [show x ++ ", " ++ show (bandGaps !! x) ++ ", " ++ show (allowedBands !! x)| x <- [0, 1 .. 20]]
+
+-- Calculate difference between groups of 2 elements in a list
+diffs :: (Num a) => [a] -> [a]
+diffs (a:b:xs) = (b - a):(diffs xs)
 
 -- getEdge takes a function and a starting point
 -- and returns the next band edge
 getEdge :: (Double -> Double) -> Double -> Double
 getEdge func start = edge
   where
-    edge = solve (\x -> equ x - (signum (func x))) (approxNextEdge func start)
+    edge = solve (\x -> simpleEqu x - (signum (func x))) (approxNextEdge func start)
 
 approxNextEdge :: (Double -> Double) -> Double -> Double
 approxNextEdge func start =
@@ -25,9 +37,11 @@ approxNextEdge func start =
     where
       edgeSign
         | startSlope == 1 && startVal >= -1 = 1
-        | startSlope == 1 && startVal < -1 = (-1)
-        | startSlope == -1 && startVal <= 1 = (-1)
+        | startSlope == 1 && startVal < -1 = -1
+        | startSlope == -1 && startVal <= 1 = -1
         | startSlope == -1 && startVal > 1 = 1
+        | startSlope == 0 && startVal > 1 = 1
+        | startSlope == 0 && startVal < 1 = -1
           where
             startVal = func start
             startSlope = round $ signum $ slope func start
--- newton.hs
+++ newton.hs
@@ -3,13 +3,14 @@ import Data.List
 import Data.Maybe
 
 -- Made up number for epsilon, probably shouldn't call it epsilon... that's confusing
-epsilon = 1e-5
+--epsilon = 1e-10
 slope :: (Double -> Double) -> Double -> Double
 slope func x = (func (x+inc) - func (x-inc))/(2*inc)
   where
-    inc = if x == 0
-            then epsilon
-            else x * epsilon
+    inc = (abs x) * 1e-10 + 1e-300
+    --inc = if x == 0
+    --        then epsilon
+    --        else x * epsilon
 
 step :: (Double -> Double) -> Double -> Double
 step func x = x - (func x) / (slope func x)
